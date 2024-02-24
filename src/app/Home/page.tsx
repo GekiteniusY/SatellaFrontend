08cc39aa861191daf9bf524ad926d08324b1ab98
GekiteniusY <genkichi.yogi@gmail.com>
2024-02-24 23:09:47 +0900

    Google Fontsの導入

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,10 +1,21 @@
-// サインイン状態でなくともアクセス可能なページ
 import { logout } from "../auth/actions";
 
 export default function Home() {
   return (
     <div>
-      Home
+      <div className='p-5 '>
+        <p className='font-mono text-5xl'>Welcome Sattela</p>
+        <p>Sattela provide your sattelite office.</p>
+      </div>
+      <div className='pt-10'>
+        <p className='text-2xl'>直近の予約</p>
+      </div>
+      <div className='pt-10'>
+        <p className='text-2xl'>Bookmark</p>
+      </div>
+      <div>User Profile Icon</div>
+      <div className='font-bizUd'>User Profile Icon</div>
+      <div className='font-bizUdB'>User Profile Icon</div>
       <br />
       <form>
         <button formAction={logout}>Logout</button>
